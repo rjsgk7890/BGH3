90790830b0eb22f3bb061d75c95c3f799494d1eb
rjsgk789 <21800345@handong.edu>
2019-04-17 17:18:26 +0900

    yy

--- main.c
+++ main.c
@@ -2,11 +2,13 @@
 
 void f1();
 void f2();
+void f3();
 
 int main(){
 
 	f1();
 	f2();
+	f3();
 
 	return 0;
 }
@@ -17,4 +19,8 @@ void f1(){
 
 void f2(){
 	printf("bbbbb\n");
+}
+
+void f3(){
+	printf("ccccc\n");
 }
